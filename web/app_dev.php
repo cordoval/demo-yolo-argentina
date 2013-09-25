32ad1755e47ff7eb9d547b18e527566afb101ecb
Luis Cordova <cordoval@gmail.com>
2013-09-25 17:34:27 -0500

    trying to setup yolo to CAS and return the exams

--- web/app_dev.php
+++ web/app_dev.php
@@ -1,2 +1,26 @@
 <?php
 
+require __DIR__.'/../vendor/autoload.php';
+
+$container = Yolo\createContainer(
+    [
+        'debug' => true,
+    ],
+    [
+        new Yolo\DependencyInjection\MonologExtension(),
+        new Yolo\DependencyInjection\ServiceControllerExtension(),
+//        new Yolo\DependencyInjection\CallableExtension(
+//            'controller',
+//            function ($configs, $container) {
+//                $container->register('hello.controller', 'HelloController')
+//                    ->addArgument('%hello.name%');
+//            }
+//        ),
+    ]
+);
+
+$app = new Yolo\Application($container);
+
+$app->get('/', 'yolo_demo_controller:indexAction');
+
+$app->run();
